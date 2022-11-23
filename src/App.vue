dc1ae3ba64de6008fb4cba36bfec49956ec819d1
Sumon400011 <sumonsm89@gmail.com>
2022-11-23 23:23:55 +0600

    card component saperated with composition api functionality

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
   import data from "./data/quizes.json";
   import { ref, watch } from "vue";
+  import Card from "./components/Card.vue"
 
   const quizes = ref(data);
   const search = ref("");
@@ -17,14 +18,9 @@
       <h1>Quizes</h1>
       <input type="text" v-model.trim="search" placeholder="Search...">
     </header>
+    <!-- v-for="quiz in quizes" :key="quiz.id" -->
     <div class="options-container">
-      <div v-for="quiz in quizes" :key="quiz.id" class="card">
-        <img :src="quiz.img" :alt="quiz.name">
-        <div class="card-text" style="padding:10px 15px;">
-          <h2>{{ quiz.name }}</h2>
-          <p>{{ quiz.questions.length }} questions</p>
-        </div>
-      </div>
+      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
     </div>
   </div>
 </template>
@@ -58,31 +54,5 @@
     display: flex;
     flex-wrap: wrap;
     margin-top: 40px;
-  }
-
-  /* CARD */
-
-  .card {
-    width: 310px;
-    overflow: hidden;
-    border-radius: 2%;
-    box-shadow: 1px 1px 10px rgba(0,0,0,0.1);
-    margin-bottom: 35px;
-    margin-right: 20px;
-    cursor: pointer;
-  }
-
-  .card img {
-    width: 100%;
-    height: 190px;
-    margin: 0
-  }
-
-  .card .card-text {
-    padding: 0 5px
-  }
-
-  .card .card-text h2 {
-    font-weight: bold;
   }
 </style>
