3f821fb2bb6b89f297daf038876d5fda0dcee0bc
Sumon400011 <sumonsm89@gmail.com>
2022-11-25 15:04:18 +0600

    quiz and question basic data fetched and functionality added

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -2,18 +2,22 @@
   <div>
     <QuizHeader />
     <div>
-      <Question/>
-      <Result />
+      <Question :question="quiz.questions[currentQuestionIndex]"/>
+      <!-- <Result /> -->
     </div>
   </div>
 </template>
 <script setup>
   import Question from "../components/Question.vue"
   import QuizHeader from "../components/QuizHeader.vue"
-  import Result from "../components/Result.vue"
-  // import quizes from "../data/quizes.json";
+  // import Result from "../components/Result.vue"
+  import quizes from "../data/quizes.json";
   import { useRoute } from 'vue-router';
+  import { ref } from "vue";
+  
   const route = useRoute();
   const quizId = parseInt(route.params.id)
-  // const quiz = quizes.find(item => item.id === quizId);
+  const quiz = quizes.find(item => item.id === quizId);
+  
+  const currentQuestionIndex = ref(0);
 </script>
