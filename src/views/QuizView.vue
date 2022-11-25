2982aa00f578747b708379a7c818bb65bf0c0794
Sumon400011 <sumonsm89@gmail.com>
2022-11-25 13:45:55 +0600

    quiz related componenets created and designed

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,12 +1,19 @@
 <template>
+  <div>
+    <QuizHeader />
     <div>
-      {{ quiz }}
+      <Question/>
+      <Result />
     </div>
+  </div>
 </template>
 <script setup>
-    import quizes from "../data/quizes.json";
-    import { useRoute } from 'vue-router';
-    const route = useRoute();
-    const quizId = parseInt(route.params.id)
-    const quiz = quizes.find(item => item.id === quizId);
+  import Question from "../components/Question.vue"
+  import QuizHeader from "../components/QuizHeader.vue"
+  import Result from "../components/Result.vue"
+  // import quizes from "../data/quizes.json";
+  import { useRoute } from 'vue-router';
+  const route = useRoute();
+  const quizId = parseInt(route.params.id)
+  // const quiz = quizes.find(item => item.id === quizId);
 </script>
